ff4400f54c34f284477c905c2953c63cce0551dc
Alessandro <alessandro2ap3@gmail.com>
2025-01-06 00:07:05 +0100

    Add note saving functionality and JSON serialization

--- note/note.go
+++ note/note.go
@@ -1,20 +1,39 @@
 package note
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"os"
+	"strings"
 	"time"
 )
 
 type Note struct {
-	title     string
-	content   string
-	createdat time.Time
+	// Define the Note struct here with the following fields
+	// with the json tags (struct tags) as shown in the test cases.
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 func (note Note) Display() {
 	// Display the note
-	fmt.Printf("Your note titled: %v has the following content:\n\n%v", note.title, note.content)
+	fmt.Printf("Your note titled: %v has the following content:\n\n%v\n\n", note.Title, note.Content)
+}
+
+func (note Note) Save() error {
+	// Save the note to a file
+	fileName := strings.ReplaceAll(note.Title, " ", "_")
+	fileName = strings.ToLower(fileName) + ".json"
+	json, err := json.Marshal(note)
+
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(fileName, json, 0644)
+
 }
 
 func New(title, content string) (Note, error) {
@@ -22,8 +41,8 @@ func New(title, content string) (Note, error) {
 		return Note{}, errors.New("input cannot be empty")
 	}
 	return Note{
-		title:     title,
-		content:   content,
-		createdat: time.Now(),
+		Title:     title,
+		Content:   content,
+		CreatedAt: time.Now(),
 	}, nil
 }
