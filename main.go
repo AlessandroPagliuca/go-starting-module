7d2144d159035b6d49e027a413ef6fbdd54d2fe4
Alessandro <alessandro2ap3@gmail.com>
2025-01-06 12:55:55 +0100

    Add printSomething function for type-specific output and update JSON files

--- main.go
+++ main.go
@@ -34,6 +34,11 @@ type Outputtable interface {
 // }
 
 func main() {
+
+	printSomething(1)
+	printSomething(1.0)
+	printSomething("Hello")
+
 	title, content := getNoteData()
 	todoText := getUserInput("Todo text: ")
 
@@ -43,6 +48,7 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+	printSomething(todo)
 
 	userNote, err := note.New(title, content)
 
@@ -59,6 +65,37 @@ func main() {
 	}
 
 	outputData(userNote)
+}
+
+func printSomething(value interface{}) {
+	intVal, ok := value.(int)
+
+	if ok {
+		fmt.Println("This is an integer", intVal)
+		return
+	}
+
+	floatVal, ok := value.(float64)
+
+	if ok {
+		fmt.Println("This is an integer", floatVal)
+		return
+	}
+
+	stringVal, ok := value.(string)
+
+	if ok {
+		fmt.Println("This is an string", stringVal)
+		return
+	}
+	// switch value.(type) {
+	// case int:
+	// 	fmt.Println("This is an integer:", value)
+	// case float64:
+	// 	fmt.Println("This is a float:", value)
+	// case string:
+	// 	fmt.Println("This is a string:", value)
+	// }
 }
 
 func outputData(data Outputtable) error {
